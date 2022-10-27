a94314dc1b40d7570948298e88638313722eebae
Kakidiako Afonso <kakidiakoafonso@gmail.com>
2022-10-27 15:45:12 +0100

    Initial Commit

---
+++ Telegram/TelegramApp.swift
@@ -0,0 +1,17 @@
+//
+//  TelegramApp.swift
+//  Telegram
+//
+//  Created by Kakidiako Afonso on 27/10/22.
+//
+
+import SwiftUI
+
+@main
+struct TelegramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
